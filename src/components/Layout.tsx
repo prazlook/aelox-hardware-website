25588881643a60ecd44cd64628db06c21bd7c508
prazlook <prazlook@gmail.com>
2025-10-10 07:34:57 +0200

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,13 +4,13 @@ import { Header } from "./Header";
 
 const Layout = () => {
   return (
-    <div className="flex h-screen bg-theme-dark text-theme-text-primary">
+    <div className="flex min-h-screen bg-theme-dark text-theme-text-primary">
       <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto p-6">
+      <div className="flex flex-col flex-1">
+        <main className="flex-1 p-6">
           <Outlet />
         </main>
+        <Header /> {/* Le Header est maintenant en bas */}
       </div>
     </div>
   );
