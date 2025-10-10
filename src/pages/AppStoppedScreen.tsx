ff408c03c224f62696b3903713b178de2247c669
prazlook <prazlook@gmail.com>
2025-10-10 08:04:12 +0200

    [dyad] Mise à jour du bouton de redémarrage et de l'écran d'arrêt - wrote 3 file(s)

--- src/pages/AppStoppedScreen.tsx
+++ src/pages/AppStoppedScreen.tsx
@@ -9,12 +9,8 @@ const AppStoppedScreen: React.FC<AppStoppedScreenProps> = ({ onRestart }) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-theme-dark text-theme-text-primary p-4">
       <div className="text-center space-y-6">
-        <p className="text-xl text-gray-400">
-          L'application est actuellement arrêtée. Veuillez la redémarrer pour continuer.
-        </p>
-        <FuturisticButton onClick={onRestart}>
-          Redémarrer l'application
-        </FuturisticButton>
+        {/* Le texte descriptif a été supprimé pour un affichage plus épuré */}
+        <FuturisticButton onClick={onRestart} />
       </div>
     </div>
   );
