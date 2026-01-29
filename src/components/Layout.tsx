6491b3849fdc5cf136d052e3c9cba8e341098ebd
[dyad] <prazlook@gmail.com>
2026-01-29 07:55:42 +0100

    Reverted all changes back to version ebc31f11f88ad703196eed711cb15e2f0bda08de

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "./Sidebar";
+import { Header } from "./Header";
 
 const Layout = () => {
   return (
     <div className="flex min-h-screen bg-theme-dark text-theme-text-primary">
       <Sidebar />
       <div className="flex flex-col flex-1">
-        <main className="flex-1 p-6 pl-20"> {/* Suppression du pb-16 car le Header est retiré */}
+        <main className="flex-1 p-6 pb-16 pl-20"> {/* Ajout de pl-20 pour décaler le contenu du sidebar */}
           <Outlet />
         </main>
       </div>
+      <Header /> {/* Le Header est maintenant un sibling direct et est fixe */}
     </div>
   );
 };
