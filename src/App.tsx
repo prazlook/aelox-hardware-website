0aeb597d7d63e8d719dff71ab9ead475ee2e3305
[dyad] <prazlook@gmail.com>
2026-01-29 08:01:37 +0100

    Reverted all changes back to version aaa5803d75f70c970540e93004eebc517e65c976

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Dashboard from './pages/Dashboard';
 import About from './pages/About';
@@ -35,9 +35,7 @@ const AppContent = () => {
 function App() {
   return (
     <AppStatusProvider>
-      <BrowserRouter>
-        <AppContent />
-      </BrowserRouter>
+      <AppContent />
     </AppStatusProvider>
   );
 }
