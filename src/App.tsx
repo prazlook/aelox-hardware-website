ad73d6954cbff6f95ef1ad843a7ef3bd021d50bd
[dyad] <prazlook@gmail.com>
2026-01-29 08:21:36 +0100

    Updated src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"; // <--- AJOUTE CET IMPORT
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
